b6183ecd25dbffb9fa156eb5ad2b98c63761dd35
PhucDev <phucvan2704@gmail.com>
2025-10-02 13:03:46 +0700

    fix: .env

--- src/main/java/org/badmintonchain/Application.java
+++ src/main/java/org/badmintonchain/Application.java
@@ -1,5 +1,6 @@
 package org.badmintonchain;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableRabbit
 public class Application {
     public static void main(String[] args) {
+        try {
+            Dotenv dotenv = Dotenv.configure()
+                    .ignoreIfMissing()
+                    .systemProperties() // put into System properties
+                    .load();
+            
+        } catch (Exception e) {
+            System.err.println("Warning: failed to load .env: " + e.getMessage());
+        }
+
         SpringApplication.run(Application.class, args);
     }
 }
